ca05b84be01336ecdf4fe46f90a463efff1949ae
rextsears <srtm88@gmail.com>
2023-08-09 11:50:36 -0400

    js updates

--- js/script.js
+++ js/script.js
@@ -1,13 +1,66 @@
-/*----- constants -----*/
+/*---->> Variables <<----*/
 
-  
-  /*----- app's state (variables) -----*/
+let holes, activePlayer, winner;
 
-  
-  /*----- cached element references -----*/
+/*---->> Cached Elements <<----*/
 
-  
-  /*----- event listeners -----*/
+let player1 = document.getElementById('player1');
+let player2 = document.getElementById('player2');
+let holeEl = document.getElementById('stdhole');
+let p1ScoreEl = document.querySelector('#scorep1');
+let p2ScoreEl = document.querySelector('#scorep2');
 
-  
-  /*----- functions -----*/
+/*---->> Event Listeners <<----*/
+
+// Click gameboard
+document.querySelector('#gameboard').addEventListener('click', gbClick);
+
+// Click 'New Game' button
+document.querySelector('#newgamebutton').addEventListener('click', function(){});
+
+/*---->> Functions <<----*/
+// New Game
+function cleanSlate() {
+
+};
+
+// Click/Interact with gameboard
+function gbClick() {
+
+};
+
+// Move pieces on gameboard
+function pieceMove {
+
+};
+
+
+// Play behavior when player is on home side
+function homeGame {
+
+};
+
+// Play behavior when player is on away side
+function awayGame {
+
+};
+
+// Change active player (change turns)
+function yourTurn {
+
+};
+
+// Calculate a winner
+function whoWon {
+
+};
+
+// Display a winner
+function showOff {
+
+};
+
+// Render gameplay on gameboard
+function render {
+    
+};
