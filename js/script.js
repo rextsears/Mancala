fa7f567d754284ba3e8aa52e75f23fb935d5f5a4
rextsears <srtm88@gmail.com>
2023-08-10 09:01:07 -0400

    js updates 8 10

--- js/script.js
+++ js/script.js
@@ -1,66 +1,144 @@
-/*---->> Variables <<----*/
+/*----- constants -----*/
+  
+/*----- app's state (variables) -----*/
+let boardHoles = [];
+let activePlayer;
+let gameWinner;
+  
+/*----- cached element references -----*/
+let scorep1 = document.getElementById("p1score");
+let scorep2 = document.getElementById("p2score");
+let hole1 = document.getElementById("hole-1");
+let hole2 = document.getElementById("hole-2");
+let hole3 = document.getElementById("hole-3");
+let hole4 = document.getElementById("hole-4");
+let hole5 = document.getElementById("hole-5");
+let hole6 = document.getElementById("hole-6");
+let hole7 = document.getElementById("hole-7");
+let hole8 = document.getElementById("hole-8");
+let hole9 = document.getElementById("hole-9");
+let hole10 = document.getElementById("hole-10");
+let hole11 = document.getElementById("hole-11");
+let hole12 = document.getElementById("hole-12");
+let player1container = document.getElementById("player-1-container");
+let player2container = document.getElementById("player-2-container");
 
-let holes, activePlayer, winner;
+  
+/*----- event listeners -----*/
+let hole1Click = hole1.addEventListener('click', function(){
+cleanSlate();
+holeNumRend(boardHoles);
+});
+ 
+let hole2Click = hole2.addEventListener('click', function(){
 
-/*---->> Cached Elements <<----*/
+});
 
-let player1 = document.getElementById('player1');
-let player2 = document.getElementById('player2');
-let holeEl = document.getElementById('stdhole');
-let p1ScoreEl = document.querySelector('#scorep1');
-let p2ScoreEl = document.querySelector('#scorep2');
 
-/*---->> Event Listeners <<----*/
+let hole3Click = hole3.addEventListener('click', function(){
 
-// Click gameboard
-document.querySelector('#gameboard').addEventListener('click', gbClick);
+});
 
-// Click 'New Game' button
-document.querySelector('#newgamebutton').addEventListener('click', function(){});
 
-/*---->> Functions <<----*/
+let hole4Click = hole4.addEventListener('click', function(){
+
+});
+
+
+let hole5Click = hole5.addEventListener('click', function(){
+
+});
+
+
+let hole6Click = hole6.addEventListener('click', function(){
+
+});
+
+
+let hole7Click = hole7.addEventListener('click', function(){
+
+});
+
+
+let hole8Click = hole8.addEventListener('click', function(){
+
+});
+
+
+let hole9Click = hole9.addEventListener('click', function(){
+
+});
+
+
+let hole10Click = hole10.addEventListener('click', function(){
+
+});
+
+
+let hole11Click = hole11.addEventListener('click', function(){
+
+});
+
+
+let hole12Click = hole12.addEventListener('click', function(){
+
+});
+ 
+
+
+/*----- functions -----*/
 // New Game
 function cleanSlate() {
+boardHoles = [4,4,4,4,4,4,0,4,4,4,4,4,4,0];
+gameWinner = null;
+return boardHoles, gameWinner;
+
 
 };
+
+// Scoreboard
+function keepingScore(x,y) {
+    let scorep1 = x + scorep1;
+    let scorep2 = y + scorep2;
+    return scorep1, scorep2;
+}
 
 // Click/Interact with gameboard
-function gbClick() {
 
-};
 
 // Move pieces on gameboard
-function pieceMove {
-
-};
 
 
 // Play behavior when player is on home side
-function homeGame {
 
-};
 
 // Play behavior when player is on away side
-function awayGame {
 
-};
 
 // Change active player (change turns)
-function yourTurn {
 
-};
 
 // Calculate a winner
-function whoWon {
 
-};
 
 // Display a winner
-function showOff {
 
-};
 
 // Render gameplay on gameboard
-function render {
-    
-};
+function holeNumRend(v) {
+        hole7.innerHTML = v[0];
+        hole8.innerHTML = v[1];
+        hole9.innerHTML = v[2];
+        hole10.innerHTML = v[3];
+        hole11.innerHTML = v[4];
+        hole12.innerHTML = v[5];
+        player1container.innerHTML = v[6];
+        hole1.innerHTML = v[7];
+        hole2.innerHTML = v[8];
+        hole3.innerHTML = v[9];
+        hole4.innerHTML = v[10];
+        hole5.innerHTML = v[11];
+        hole6.innerHTML = v[12];
+        player2container.innerHTML = v[13];
+}
+
